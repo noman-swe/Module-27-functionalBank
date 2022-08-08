cde1a7e982b2f15830b84254f205210de5c8b464
noman-swe <noman.swe130@gmail.com>
2022-08-09 00:08:28 +0600

    27-2 Get User deposit and set deposit total value

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,15 @@
-console.log('connected');
+document.getElementById('deposit-btn').addEventListener('click', () => {
+    const depositInput = document.getElementById('deposit-input');
+    const depositInputText = depositInput.value;
+    const depositInputAmount = parseFloat(depositInputText);
+
+    // set value to deposit card
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const depositTotalAmount = parseFloat(depositTotalText);
+    const totalDepositAmount = depositTotalAmount + depositInputAmount;
+    depositTotal.innerText = totalDepositAmount;
+
+    // set the input field empty
+    depositInput.value = '';
+});
