65b84221d533fd2a53549bd3af2401ee7a14d703
noman-swe <noman.swe130@gmail.com>
2022-08-09 00:57:41 +0600

    27-4 Money withdraw and reduce balance for withdraw

--- js/banking.js
+++ js/banking.js
@@ -20,3 +20,28 @@ document.getElementById('deposit-btn').addEventListener('click', () => {
     // set the input field empty
     depositInput.value = '';
 });
+
+
+// withdraw
+document.getElementById('withdraw-btn').addEventListener('click', () => {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const dwithdrawInputText = withdrawInput.value;
+    const withdrawInputAmount = parseFloat(dwithdrawInputText);
+
+    // set value to deposit card
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const prviousWithdrawTotalAmount = parseFloat(withdrawTotalText);
+    const prviousTotalWithdrawAmount = prviousWithdrawTotalAmount + withdrawInputAmount;
+    withdrawTotal.innerText = prviousTotalWithdrawAmount;
+
+    // deposit add to balance
+    const totalBalanceField = document.getElementById('balance-total');
+    const totalBalanceText = totalBalanceField.innerText;
+    const perviousBalanceTotal = parseFloat(totalBalanceText);
+    const totalBalance = perviousBalanceTotal - withdrawInputAmount;
+    totalBalanceField.innerText = totalBalance;
+
+    // set the input field empty
+    withdrawInput.value = '';
+});
